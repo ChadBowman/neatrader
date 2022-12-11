0ed33ff5d5f1cf4d4cb1a27a11e59b2342328b58
Chad Bowman <chad.bowman0@gmail.com>
2022-12-10 18:51:44 -0700

    use importlib to source data resources, move resources to resource folder, create graphs after two gen iterations

--- neatrader/neatrader.py
+++ neatrader/neatrader.py
@@ -1,5 +1,6 @@
 import neat
 import pandas as pd
+import os
 from glob import glob
 import neatrader.visualize as vis
 from neatrader.model import Portfolio, Security
@@ -9,12 +10,21 @@ from neatrader.utils import from_small_date
 from neatrader.reporter import TradeReporter
 from pathlib import Path
 from multiprocessing import Process, Manager
-
+from importlib import resources
 
 TSLA = Security('TSLA')
-path = Path('normalized') / TSLA.symbol
-training = pd.read_csv(path / 'training.csv', parse_dates=['date'], date_parser=from_small_date)
-validation = pd.read_csv(path / 'cross_validation.csv', parse_dates=['date'], date_parser=from_small_date)
+
+
+def find_data_directory_path():
+    path = None
+    with resources.path("resources.normalized", "TSLA") as fp:
+        path = Path(fp)
+    return path
+
+
+path = find_data_directory_path()
+training = pd.read_csv(path / "training.csv", parse_dates=['date'], date_parser=from_small_date)
+validation = pd.read_csv(path / "cross_validation.csv", parse_dates=['date'], date_parser=from_small_date)
 training_daterange_factory = DateRangeFactory(training)
 cv_daterange_factory = DateRangeFactory(validation)
 
@@ -24,6 +34,7 @@ def worker(mode, simulator, net, start, end, return_dict):
 
 
 def eval_genomes(genomes, config):
+
     # all genomes should be compared against using the same date range
     t_start, t_end = training_daterange_factory.random_date_range(90)
     c_start, c_end = cv_daterange_factory.random_date_range(90)
@@ -47,44 +58,53 @@ def eval_genomes(genomes, config):
 
 
 def run(config_file):
-    config = neat.Config(
-        neat.DefaultGenome,
-        neat.DefaultReproduction,
-        neat.DefaultSpeciesSet,
-        neat.DefaultStagnation,
-        config_file
-    )
+    try:
+        config = neat.Config(
+            neat.DefaultGenome,
+            neat.DefaultReproduction,
+            neat.DefaultSpeciesSet,
+            neat.DefaultStagnation,
+            config_file
+        )
 
-    while True:
-        checkpoint = sorted(glob('neat-checkpoint-*'), reverse=True)
-        if checkpoint:
-            pop = neat.Checkpointer.restore_checkpoint(checkpoint[0])
-        else:
-            pop = neat.Population(config)
+        while True:
+            checkpoint = sorted(glob('neat-checkpoint-*'), reverse=True)
+            if checkpoint:
+                pop = neat.Checkpointer.restore_checkpoint(checkpoint[0])
+            else:
+                pop = neat.Population(config)
 
-        stats = neat.StatisticsReporter()
+            stats = neat.StatisticsReporter()
 
-        # add a stdout reporter to show progress in terminal
-        pop.add_reporter(neat.StdOutReporter(True))
-        pop.add_reporter(stats)
-        pop.add_reporter(neat.Checkpointer(50))
+            # add a stdout reporter to show progress in terminal
+            pop.add_reporter(neat.StdOutReporter(True))
+            pop.add_reporter(stats)
+            pop.add_reporter(neat.Checkpointer(1))
 
-        winner = pop.run(eval_genomes, 50)
+            winner = pop.run(eval_genomes, 2)
 
-        # display the winning genome
-        print(f"\nBest genome:\n{winner}")
+            # display the winning genome
+            print(f"\nBest genome:\n{winner}")
 
-        win_net = neat.nn.FeedForwardNetwork.create(winner, config)
+            win_net = neat.nn.FeedForwardNetwork.create(winner, config)
 
-        view = False
-        vis.plot_stats(stats, ylog=False, view=view)
-        vis.plot_species(stats, view=view)
+            view = False
+            vis.plot_stats(stats, ylog=False, view=view)
+            vis.plot_species(stats, view=view)
 
-        portfolio = Portfolio(cash=0, securities={TSLA: 100})
-        simulator = Simulator(TSLA, portfolio, path, training)
-        daterange = training_daterange_factory.random_date_range(90)
-        reporter = TradeReporter()
-        vis.plot_trades(win_net, simulator, daterange, training, path, reporter, view=view)
+            portfolio = Portfolio(cash=0, securities={TSLA: 100})
+            simulator = Simulator(TSLA, portfolio, path, training)
+            daterange = training_daterange_factory.random_date_range(90)
+            reporter = TradeReporter()
+            vis.plot_trades(win_net, simulator, daterange, training, path, reporter, view=view)
 
-        node_names = {0: 'Buy', 1: 'Sell', 2: 'Hold', 3: 'Delta', 4: 'Theta'}
-        vis.draw_net(config, winner, view=view, node_names=node_names)
+            node_names = {
+                -1: 'cash', -2: 'shares',  -3: 'close', -4: 'macd', -5: 'macd_signal',
+                -6: 'macd_diff', -7: 'bb_bbh', -8: 'bb_bbh', -9: 'bb_bbl', -10: 'rsi',
+                0: 'Buy', 1: 'Sell', 2: 'Hold', 3: 'Delta', 4: 'Theta'
+            }
+            vis.draw_net(config, winner, view=view, node_names=node_names)
+    finally:
+        checkpoints = sorted(glob('neat-checkpoint-*'), reverse=True)
+        for checkpoint in checkpoints[1:]:
+            os.remove(checkpoint)
