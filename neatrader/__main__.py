a9b419b5be847846e17f69431f1e712f94d13617
Chad Bowman <chad.bowman0@gmail.com>
2022-12-21 20:41:48 -0700

    supress WARNs

--- neatrader/__main__.py
+++ neatrader/__main__.py
@@ -1,10 +1,12 @@
+import logging
+import math
 import os
 import sys
-import math
 from neatrader import neatrader
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.ERROR)
     config_path = os.path.join('neatrader', 'config.ini')
     generations_per_iteration = int(sys.argv[1]) if len(sys.argv) > 1 else 3
     iterations = int(sys.argv[2]) if len(sys.argv) > 2 else math.inf
